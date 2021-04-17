bd9c4ddfac04cfa6ffbc588ab4d1d54ac27bb1fb
GantGanz <anggerelegant7@gmail.com>
2021-04-17 14:00:39 +0700

    mengubah+menghapus catatan

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const {
     addNoteHandler,
     getAllNotesHandler,
     getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
 } = require('./handler');
 
 const routes = [{
@@ -16,6 +18,14 @@ const routes = [{
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
+}, {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+}, {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
 }];
 
 module.exports = routes;
